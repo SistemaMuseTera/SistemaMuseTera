a60ddc99dc43ce050b5971caf6626a79cc9f371e
karlos123230 <admin@musicoterapia.com>
2025-01-10 17:40:32 -0300

    feat: Adiciona função formatarData e corrige importação no PlanoTerapeuticoPDF

--- src/components/processos/PlanoTerapeuticoPDF.tsx
+++ src/components/processos/PlanoTerapeuticoPDF.tsx
@@ -1,4 +1,6 @@
+import { formatarData } from '@/lib/utils'
+
 <div className="text-right">
   <p>${formatarData(new Date())}</p>
   <p>MuseTera</p>
-</div> 
+</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatarData(data: Date): string {
+  return data.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  })
+}
